0090369658dffde2ba020b3bb000cf917a9a67f3
Daniel Gaspar Gonçalves <daniel.gaspar.goncalves@gmail.com>
2021-11-25 00:57:28 -0300

    Insert new complaint into db

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express()
 const mysql = require('mysql')
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
@@ -19,15 +19,37 @@ const db = mysql.createPool({
     res.send("hello world")
 }); */
 
+/* get complaint list */
 app.get('/api/complaint', (req, res) => {
     const sqlSelect = 'SELECT * FROM complaints';
     db.query(sqlSelect, (err, result) => {
         res.send(result);
-        /* console.log('AAAAAAAAAAAA'); */
-        console.log(result);
-        /* console.log('BBBBBBBBBBBB'); */
+        /* console.log(result); */
     });
 })
+
+/* insert new complaint */
+app.post('/api/complaint/insert', (req, res) => {
+    const complaintID = req.body.complaintID
+    const cityID = req.body.cityID
+    const ip = '123456'
+    const street = req.body.street
+    const neighborhood = req.body.neighborhood
+
+    console.log('INSERT ' + complaintID + ', ' + cityID + ', ' + ip + ', ' + street + ', ' + neighborhood);
+    const sqlInsert =
+        "INSERT INTO `complaint_register` (`complaint_id`, `city_id`, `ip`, `date`, `street`, `neighborhood`) VALUES (?, ?, ?, CURDATE(), ?, ?)"
+    db.query(sqlInsert, [complaintID, cityID, ip, street, neighborhood], (err, result) => {
+        res.send("QQQQQQQQQQQQQQQQQQQQQ");
+    })
+})
+/* app.get('/newComplaint', (req, res) => {
+    const sqlInsert = 
+        "INSERT INTO `complaint_register` (`complaint_id`, `city_id`, `ip`, `date`, `street`, `neighborhood`) VALUES ('1', '303762', '123456', CURDATE(), 'RUA JOSÉ', 'BAIRRO DO CHICO')"
+    db.query(sqlInsert, (err, result) => {
+        res.send("QQQQQQQQQQQQQQQQQQQQQ");
+    })
+}) */
 
 /* app.post('/api/insert', (reqPost, resPost) => {
     const latitude = reqPost.body.latitude;
@@ -41,13 +63,14 @@ app.route('/api/city')
         const latitude = req.body.latitude;
         const longitude = req.body.longitude;
 
-        console.log('data received from client: ' + latitude + ' ' + longitude);
+        /* console.log('data received from client: ' + latitude + ' ' + longitude); */
     }).get((req, res) => {
         /* search city given latitude and longitude */
         const latitude = req.query.latitude
         const longitude = req.query.longitude
         console.log('data received from client: ' + latitude + ' ' + longitude);
 
+        var cityID = 'ID'
         var city = 'CIDADE'
         var state = 'ESTADO'
 
@@ -60,11 +83,11 @@ app.route('/api/city')
                 LONGITUDE,
                 (
                 6371 *
-                    acos(cos(radians(` + latitude + `)) * 
+                    acos(cos(radians(?)) * 
                     cos(radians(LATITUDE)) * 
                     cos(radians(LONGITUDE) - 
-                    radians(` + longitude + `)) + 
-                    sin(radians(` + latitude + `)) * 
+                    radians(?)) + 
+                    sin(radians(?)) * 
                     sin(radians(LATITUDE)))
                 ) as dist_km
             FROM
@@ -72,18 +95,19 @@ app.route('/api/city')
             ORDER BY dist_km
             LIMIT 1
             `;
-        db.query(sqlSelect, (err, result) => {
+        db.query(sqlSelect, [latitude, longitude, latitude], (err, result) => {
             if (result.length > 0) {
                 city = result[0].nome
+                cityID = result[0].id_cidade
                 /* res.send({ city: result[0].nome, state: state }) */
-                console.log(result[0])
+                /* console.log(result[0]) */
             } else {
                 console.log(result.length + ' results')
             }
 
-            res.send({ city: city, state: state })
+            res.send({ id: cityID, city: city, state: state })
         })
-        console.log(sqlSelect);
+        /* console.log(sqlSelect); */
 
         /* res.send({city: city, state: state}); */
     })
